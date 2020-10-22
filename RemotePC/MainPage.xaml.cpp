640301ba7be7eb6f5f3a298a2979dca9270256fd
mark <marksanoj.joseph@prulitics.com>
2020-10-22 14:22:56 +0530

    change 4

--- RemotePC/MainPage.xaml.cpp
+++ RemotePC/MainPage.xaml.cpp
@@ -5,7 +5,16 @@
 
 #include "pch.h"
 #include "MainPage.xaml.h"
-
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
+using std::ios;
 using namespace RemotePC;
 
 using namespace Platform;
@@ -23,4 +32,6 @@ using namespace Windows::UI::Xaml::Navigation;
 
 MainPage::MainPage()
 {
+	InitializeComponent();
+	cout << "\nEnter Account Number: ";
 }
